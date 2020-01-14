0bfcdd71aeb2509f6e69521ad8e354f97fbfc45d
Eric Buess <ericbuess@gmail.com>
2020-01-14 16:53:51 -0600

    Initial Commit

---
+++ Review/ReviewView.h
@@ -0,0 +1,13 @@
+//
+//  ReviewView.h
+//  Review
+//
+//  Created by Eric Buess on 1/14/20.
+//  Copyright © 2020 Eric Buess. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ReviewView : ScreenSaverView
+
+@end
